507d430553ecd9aea5a7ba2358ca08a948706395
A728238 <toshiya.ito@toppan.co.jp>
2026-07-26 10:08:55 +0900

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,42 +1,4 @@
-/*! coi-serviceworker v0.1.7 | MIT License | https://github.com */
-if (typeof window === "undefined") {
-    self.addEventListener("install", () => self.skipWaiting());
-    self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
-    self.addEventListener("fetch", (e) => {
-        if (e.request.cache === "only-if-cached" && e.request.mode !== "same-origin") return;
-        e.respondWith(
-            fetch(e.request)
-                .then((response) => {
-                    if (response.status === 0) return response;
-                    const newHeaders = new Headers(response.headers);
-                    newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                    newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                    return new Response(response.body, {
-                        status: response.status,
-                        statusText: response.statusText,
-                        headers: newHeaders,
-                    });
-                })
-                .catch((e) => console.error(e))
-        );
-    });
-} else {
-    (() => {
-        const script = document.currentScript;
-        if (localStorage.getItem("coiReloaded")) {
-            localStorage.removeItem("coiReloaded");
-        } else if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register(window.location.pathname + "coi-serviceworker.js")
-                .then((registration) => {
-                    registration.addEventListener("updatefound", () => {
-                        localStorage.setItem("coiReloaded", "true");
-                        window.location.reload();
-                    });
-                    if (registration.active && !navigator.serviceWorker.controller) {
-                        localStorage.setItem("coiReloaded", "true");
-                        window.location.reload();
-                    }
-                });
-        }
-    })();
-}
+// coi-serviceworker.js の中身をこれだけにしてください
+self.addEventListener('activate', e => {
+    self.registration.unregister().then(() => self.clients.claim());
+});
